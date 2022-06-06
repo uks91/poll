d431a26615a81115d9e174beb523d01b2db18a4a
ysivanushkin <ysivanushkin@skr.local>
2022-06-06 10:12:05 +0300

    client init

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,25 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {BrowserRouter} from "react-router-dom";
-import AppRouter from "./components/AppRouter";
-import NavBar from "./components/NavBar";
-import {observer} from "mobx-react-lite";
-import {Context} from "./index";
-import {check} from "./http/userAPI";
-import {Spinner} from "react-bootstrap";
+import logo from './logo.svg';
+import './App.css';
 
-const App = observer(() => {
-    const {user} = useContext(Context)
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        check().then(data => {
-            user.setUser(true)
-            user.setIsAuth(true)
-        }).finally(() => setLoading(false))
-    }, [])
-
-    if (loading) {
-        return <Spinner animation={"grow"}/>
-    }
-
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <AppRouter />
-        </BrowserRouter>
-    );
-});
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
