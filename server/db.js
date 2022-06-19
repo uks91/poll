e5e6187d159316e2ec58b7ed94ac30b2d03508c7
Юрий Иванушкин <uranium91@mail.ru>
2022-06-19 16:27:42 +0300

    added sqlite support

--- server/db.js
+++ server/db.js
@@ -1,12 +1,28 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME, // Название БД
-    process.env.DB_USER, // Пользователь
-    process.env.DB_PASSWORD, // ПАРОЛЬ
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+if (process.env.DB_DIALECT == 'postgres') {
+
+    module.exports = new Sequelize(
+        process.env.DB_NAME, // Название БД
+        process.env.DB_USER, // Пользователь
+        process.env.DB_PASSWORD, // ПАРОЛЬ
+        {
+            dialect: 'postgres',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT
+        }
+    )
+}
+else if (process.env.DB_DIALECT == 'sqlite') {
+
+    module.exports = new Sequelize(
+        process.env.DB_NAME, // Название БД
+        process.env.DB_USER, // Пользователь
+        process.env.DB_PASSWORD, // ПАРОЛЬ
+        {
+            dialect: 'sqlite',
+            storage: "db/123.sqlite"
+        }
+
+    )
+}
